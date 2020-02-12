f4cf6d48ebcbdf2e1679b3f7420eba4f05376efd
Alberto <alberto.soutullo@bsc.es>
2020-02-12 15:12:27 +0100

    Renamed step.py to PELEStepTypes.py

--- PELETools/TimeCalculator/Step.py
+++
@@ -1,8 +0,0 @@
-# Repo Imports
-from PELETools.TimeCalculator.TimeStructures import TimeStructure
-
-class Step(TimeStructure):
-
-    def __str__(self):
-        return "PELE Step time: " + \
-               "---Average time: " +
