e994f9f5051bce9437fb21b385f0b6cc2c7cdb70
Alberto <alberto.soutullo@bsc.es>
2020-02-12 15:14:55 +0100

    Refactor TImeCalculatorArfParser Some functions are now private.

--- PELETools/TimeCalculator/TimeCalculatorArgParser.py
+++ PELETools/TimeCalculator/TimeCalculatorArgParser.py
@@ -2,7 +2,16 @@
 import argparse as ap
 
 
-def str2bool(v):
+def parse_arguments():
+    parser = ap.ArgumentParser()
+    __set_required_arguments(parser)
+    __set_optional_arguments(parser)
+    args = parser.parse_args()
+
+    return args.output_path, args.all_times
+
+
+def __str2bool(v):
     if isinstance(v, bool):
         return v
     if v.lower() in ('yes', 'true', 'True', 't', 'y', '1'):
@@ -13,7 +22,7 @@ def str2bool(v):
         raise ap.ArgumentTypeError('Boolean value expected.')
 
 
-def set_required_arguments(argument_parser: ap.ArgumentParser) -> ap.ArgumentParser:
+def __set_required_arguments(argument_parser: ap.ArgumentParser) -> ap.ArgumentParser:
     required_arguments = argument_parser.add_argument_group("Required arguments")
     required_arguments.add_argument("-p", "--output_path", required=True,
                                     metavar="Output Simulation Path", type=str,
@@ -22,20 +31,11 @@ def set_required_arguments(argument_parser: ap.ArgumentParser) -> ap.ArgumentPar
     return argument_parser
 
 
-def set_optional_arguments(argument_parser: ap.ArgumentParser) -> ap.ArgumentParser:
+def __set_optional_arguments(argument_parser: ap.ArgumentParser) -> ap.ArgumentParser:
     optional_arguments = argument_parser.add_argument_group("Optional arguments")
     optional_arguments.add_argument("-a", "--all_times", required=False, nargs='?',
                                     metavar="Enable calculation of times of a PELE Simulation",
-                                    type=str2bool, const=True, default=False,
+                                    type=__str2bool, const=True, default=False,
                                     help="Activates calculation of all PELE parts")
 
     return argument_parser
-
-
-def parse_arguments():
-    parser = ap.ArgumentParser()
-    set_required_arguments(parser)
-    set_optional_arguments(parser)
-    args = parser.parse_args()
-
-    return args.output_path, args.all_times
