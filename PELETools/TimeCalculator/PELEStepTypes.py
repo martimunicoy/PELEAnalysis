3b2c35a97af0f25bf2273e08fd650bfec5569ee5
Alberto <alberto.soutullo@bsc.es>
2020-02-12 17:53:14 +0100

    Refactor of PELEStepTypes

--- PELETools/TimeCalculator/PELEStepTypes.py
+++ PELETools/TimeCalculator/PELEStepTypes.py
@@ -5,56 +5,66 @@ from PELETools.TimeCalculator.TimeStructures import TimeStructure
 class Step(TimeStructure):
 
     name = "Step"
+    name_to_search_in_log_file = "step time"
+    time_position_in_log_file = 0
 
     def __str__(self):
-        return "PELE " + self.name + " time: " + \
-               "---Average time: " + str(self.calculate_average()) + \
-               "---Variance time: " + str(self.calculate_variance()) + \
-               "---Standard Deviation: " + str(self.calculate_standard_deviation())
+        return "PELE " + self.name + " time: \n" + \
+               "---Average time: " + str(self.calculate_average()) + "\n" \
+               "---Variance time: " + str(self.calculate_variance()) + "\n" \
+               "---Standard Deviation: " + str(self.calculate_standard_deviation()) + "\n"
 
 
 class Perturbation(TimeStructure):
 
     name = "Perturbation"
+    name_to_search_in_log_file = "Perturbation Ef:"
+    time_position_in_log_file = -1  # Last position
 
     def __str__(self):
-        return self.name + " time: " + \
-               "---Average time: " + str(self.calculate_average()) + \
-               "---Variance time: " + str(self.calculate_variance()) + \
-               "---Standard Deviation: " + str(self.calculate_standard_deviation())
+        return self.name + " time: \n" + \
+               "---Average time: " + str(self.calculate_average()) + "\n"\
+               "---Variance time: " + str(self.calculate_variance()) + "\n"\
+               "---Standard Deviation: " + str(self.calculate_standard_deviation()) + "\n"
 
 
 class ANM(TimeStructure):
 
     name = "ANM"
+    name_to_search_in_log_file = "ANM Ef:"
+    time_position_in_log_file = -1  # Last position
 
     def __str__(self):
-        return self.name + " time: " + \
-               "---Average time: " + str(self.calculate_average()) + \
-               "---Variance time: " + str(self.calculate_variance()) + \
-               "---Standard Deviation: " + str(self.calculate_standard_deviation())
+        return self.name + " time: \n" + \
+               "---Average time: " + str(self.calculate_average()) + "\n"\
+               "---Variance time: " + str(self.calculate_variance()) + "\n"\
+               "---Standard Deviation: " + str(self.calculate_standard_deviation()) + "\n"
 
 
 class SideChain(TimeStructure):
 
     name = "SideChain"
+    name_to_search_in_log_file = "Side Chain Prediction Ef"
+    time_position_in_log_file = -1  # Last position
 
     def __str__(self):
-        return self.name + " time: " + \
-               "---Average time: " + str(self.calculate_average()) + \
-               "---Variance time: " + str(self.calculate_variance()) + \
-               "---Standard Deviation: " + str(self.calculate_standard_deviation())
+        return self.name + " time: \n" + \
+               "---Average time: " + str(self.calculate_average()) + "\n"\
+               "---Variance time: " + str(self.calculate_variance()) + "\n"\
+               "---Standard Deviation: " + str(self.calculate_standard_deviation()) + "\n"
 
 
 class Minimization(TimeStructure):
 
     name = "Minimization"
+    name_to_search_in_log_file = "Minimization Ef:"
+    time_position_in_log_file = -1  # Last position
 
     def __str__(self):
-        return self.name + " time: " + \
-               "---Average time: " + str(self.calculate_average()) + \
-               "---Variance time: " + str(self.calculate_variance()) + \
-               "---Standard Deviation: " + str(self.calculate_standard_deviation())
+        return self.name + " time: \n" + \
+               "---Average time: " + str(self.calculate_average()) + "\n"\
+               "---Variance time: " + str(self.calculate_variance()) + "\n"\
+               "---Standard Deviation: " + str(self.calculate_standard_deviation()) + "\n"
 
 
 PELE_STEP_TYPES = [Step, Perturbation, ANM, SideChain, Minimization]
