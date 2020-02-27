f419f531ae00e2cd05a5cd570bf4444178c7fa94
Alberto <alberto.soutullo@bsc.es>
2020-02-27 15:24:03 +0100

    Extended tests in testTimeCalculator

--- PELETools/TimeCalculator/tests/test_TimeCalculator.py
+++ PELETools/TimeCalculator/tests/test_TimeCalculator.py
@@ -4,7 +4,9 @@ from PELETools.TimeCalculator.TimeCalculator import *
 # Libraries imports
 from nose.tools import assert_equal, assert_true, assert_in, assert_almost_equal
 
-FILES = ["data/logFile_1.txt", "data/logFile_2.txt", "data/logFile_3.txt"]
+FILES = ["PELETools/TimeCalculator/tests/data/logFile_1.txt",
+         "PELETools/TimeCalculator/tests/data/logFile_2.txt",
+         "PELETools/TimeCalculator/tests/data/logFile_3.txt"]
 
 
 class TestTimeCalculator:
@@ -50,9 +52,9 @@ class TestTimeCalculator:
         assert_equal(step_dict.total_time, 643.229)
         assert_equal(step_dict.occurrences, 6)
         assert_equal(step_dict.lowest_time, 72.389)
-        assert_equal(step_dict.lowest_time_file_path, "data/logFile_3.txt")
+        assert_equal(step_dict.lowest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_3.txt")
         assert_equal(step_dict.highest_time, 146.979)
-        assert_equal(step_dict.highest_time_file_path, "data/logFile_1.txt")
+        assert_equal(step_dict.highest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_1.txt")
 
     # def test_calculate_times_two_types(self): todo: check with only ANM and Perturbation
 
@@ -75,40 +77,44 @@ class TestTimeCalculator:
     def assert_step(self, step_dict):
         assert_almost_equal(step_dict.total_time, 643.229)
         assert_equal(step_dict.occurrences, 6)
-        # todo media
+        assert_almost_equal(step_dict.calculate_average(), 107.20483333)
         assert_almost_equal(step_dict.lowest_time, 72.389)
-        assert_equal(step_dict.lowest_time_file_path, "data/logFile_3.txt")
+        assert_equal(step_dict.lowest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_3.txt")
         assert_almost_equal(step_dict.highest_time, 146.979)
-        assert_equal(step_dict.highest_time_file_path, "data/logFile_1.txt")
+        assert_equal(step_dict.highest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_1.txt")
 
     def assert_perturbation(self, perturbation_dict):
         assert_almost_equal(perturbation_dict.total_time, 262.748)
         assert_equal(perturbation_dict.occurrences, 6)
+        assert_almost_equal(perturbation_dict.calculate_average(), 43.7913333)
         assert_almost_equal(perturbation_dict.lowest_time, 31.8809)
-        assert_equal(perturbation_dict.lowest_time_file_path, "data/logFile_3.txt")
+        assert_equal(perturbation_dict.lowest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_3.txt")
         assert_almost_equal(perturbation_dict.highest_time, 50.7186)
-        assert_equal(perturbation_dict.highest_time_file_path, "data/logFile_1.txt")
+        assert_equal(perturbation_dict.highest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_1.txt")
 
     def assert_anm(self, anm_dict):
         assert_almost_equal(anm_dict.total_time, 56.1455)
         assert_equal(anm_dict.occurrences, 3)
+        assert_almost_equal(anm_dict.calculate_average(), 18.715166666)
         assert_almost_equal(anm_dict.lowest_time, 16.8607)
-        assert_equal(anm_dict.lowest_time_file_path, "data/logFile_3.txt")
+        assert_equal(anm_dict.lowest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_3.txt")
         assert_almost_equal(anm_dict.highest_time, 21.7559)
-        assert_equal(anm_dict.highest_time_file_path, "data/logFile_1.txt")
+        assert_equal(anm_dict.highest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_1.txt")
 
     def assert_side_chain(self, side_chain_dict):
         assert_almost_equal(side_chain_dict.total_time, 112.3994)
         assert_equal(side_chain_dict.occurrences, 3)
+        assert_almost_equal(side_chain_dict.calculate_average(), 37.46646666)
         assert_almost_equal(side_chain_dict.lowest_time, 26.2207)
-        assert_equal(side_chain_dict.lowest_time_file_path, "data/logFile_2.txt")
+        assert_equal(side_chain_dict.lowest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_2.txt")
         assert_almost_equal(side_chain_dict.highest_time, 53.6631)
-        assert_equal(side_chain_dict.highest_time_file_path, "data/logFile_1.txt")
+        assert_equal(side_chain_dict.highest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_1.txt")
 
     def assert_minimization(self, minimization_dict):
         assert_almost_equal(minimization_dict.total_time, 160.8253)
         assert_equal(minimization_dict.occurrences, 6)
+        assert_almost_equal(minimization_dict.calculate_average(), 26.80421666)
         assert_almost_equal(minimization_dict.lowest_time, 21.015)
-        assert_equal(minimization_dict.lowest_time_file_path, "data/logFile_1.txt")
+        assert_equal(minimization_dict.lowest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_1.txt")
         assert_almost_equal(minimization_dict.highest_time, 32.5825)
-        assert_equal(minimization_dict.highest_time_file_path, "data/logFile_2.txt")
+        assert_equal(minimization_dict.highest_time_file_path, "PELETools/TimeCalculator/tests/data/logFile_2.txt")
