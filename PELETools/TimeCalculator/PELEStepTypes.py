2bf2bf10204959fce2be1c488c7b1f375763a73f
Alberto <alberto.soutullo@bsc.es>
2020-03-09 15:50:39 +0100

    Added Water Perturbation to step types

--- PELETools/TimeCalculator/PELEStepTypes.py
+++ PELETools/TimeCalculator/PELEStepTypes.py
@@ -6,37 +6,44 @@ class Step(TimeStructure):
 
     name = "Step"
     name_to_search_in_log_file = "step time"
-    time_position_in_log_file = 0
+    time_position_in_log_file = 0  # Last number position in logfile line
 
 
 class Perturbation(TimeStructure):
 
     name = "Perturbation"
     name_to_search_in_log_file = "Perturbation Ef:"
-    time_position_in_log_file = -1  # Last position
+    time_position_in_log_file = -1  # Last number position in logfile line
+
+
+class WaterPerturbation(TimeStructure):
+    name = "Water Perturbation"
+    name_to_search_in_log_file = "Water perturbation: Ef:"
+    time_position_in_log_file = -1  # Last number position in logfile line
 
 
 class ANM(TimeStructure):
 
     name = "ANM"
     name_to_search_in_log_file = "ANM Ef:"
-    time_position_in_log_file = -1  # Last position
+    time_position_in_log_file = -1  # Last number position in logfile line
 
 
 class SideChain(TimeStructure):
 
     name = "SideChain"
     name_to_search_in_log_file = "Side Chain Prediction Ef"
-    time_position_in_log_file = -1  # Last position
+    time_position_in_log_file = -1  # Last number position in logfile line
 
 
 class Minimization(TimeStructure):
 
     name = "Minimization"
     name_to_search_in_log_file = "Minimization Ef:"
-    time_position_in_log_file = -1  # Last position
+    time_position_in_log_file = -1  # Last number position in logfile line
 
 
 PELE_STEP_TYPES_DICT = {"Step": Step, "Perturbation": Perturbation,
+                        "WaterPerturbation" : WaterPerturbation,
                         "ANM": ANM, "SideChain": SideChain,
                         "Minimization": Minimization}
